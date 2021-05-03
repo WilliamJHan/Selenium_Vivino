b71110875ebe86603037760e87468d757fb8de8c
William Han <rbals0282@gmail.com>
2021-05-03 11:09:02 -0700

    Start Project

---
+++ vivino_starter.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver = webdriver.Chrome(r'C:\Users\JeongwooHan\Desktop\ChromeDriver\chromedriver.exe')
+driver.get(https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1NTBQS660DQ1WSwYSLmoFQNn0NNuyxKLM1JLEHLX8JFu1fFu18pLoWKAMmDKCUMZQngmEhlLmasW2iVUARfgfyg%3D%3D)
+

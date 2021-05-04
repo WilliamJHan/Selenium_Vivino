17af95b1c69e00a97bb161ec1a261bbbd9718239
William Han <rbals0282@gmail.com>
2021-05-04 15:14:45 -0700

    Finalize One Link Coding

--- vivino_final_csv.py
+++ vivino_final_csv.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import csv
 import re
 import time
@@ -10,109 +11,139 @@ import time
 driver = webdriver.Chrome('C:\\Users\\JeongwooHan\\Desktop\\ChromeDriver\\chromedriver.exe')
 
 # Go to the page to scrape
-driver.get("https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1NTBQS660DQ1WSwYSLmoFQNn0NNuyxKLM1JLEHLX8JFu1fFu18pLoWKAMmDKCUMZQngmEhlLmasW2iVUARfgfyg%3D%3D")
+driver.get("https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1MTVQS660DQ1WSwYSLmoFQNn0NNuyxKLM1JLEHLX8JFu1fFu18pLoWKBMsW1iFQCWlBR6")
 
+# Set up action chains
 actions = ActionChains(driver)
 
-# Click each wine to go to specific wine page
-wine_button = driver.find_element_by_xpath('//div[@class="vintageTitle__vintageTitle--2iCdc"]/a').get_attribute('href')
-wine_button.click()
+# Scroll to the bottom
+# SCROLL_PAUSE_TIME = 1
+# last_height = driver.execute_script("return document.body.scrollHeight")
+# while True:
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     time.sleep(SCROLL_PAUSE_TIME)
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         break
+#     last_height = new_height
 
+# Set up CSV
+csv_file = open('vivino_wine.csv', 'w', newline = '')
+fieldnames = ['winery','wine_type','city','country','wine_name','year','overall_rating','overall_rating_count',\
+              'price','light_bold','smooth_tannic','dry_sweet','soft_acidic']
+writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
 
-wine_button.switch_to.window(wine_button.window_handles[-1])
-wine_button.close()
+# Get all urls
+all_urls = driver.find_elements_by_xpath(".//div[@class='explorerPage__results--3wqLw']/div/div")
+one_url = all_urls[0]
+url = one_url.find_element_by_xpath("./div/div/a")
+all_str_urls = [url.find_element_by_xpath("./div/div/a").get_attribute('href') for url in all_urls]
 
-csv_file = open('vivino_wine.csv', 'w', encoding = 'utf-8', newline = '')
-writer = csv.writer(csv_file)
+index = 1
+for url in all_str_urls:
+    print("Scraping Item Number: " + str(index))
+    index = index + 1
+    driver.get(url)
+    #Pull different elements using xpath
+    try:
+        winery = driver.find_element_by_xpath('//span[@class="headline"]/a[@class="winery"]').text
+    except Exception as e:
+        print(e)
+        
+    try:
+        wine_type = driver.find_element_by_xpath('//span[@class="wineLocationHeader__wineType--14nrC"]').text
+    except Exception as e:
+        print(e)
+        
+    try:
+        city = driver.find_element_by_xpath('//a[@class="anchor__anchor--3DOSm wineLocationHeader__region--1cbip"]').text
+    except Exception as e:
+        print(e)
+        
+    try:
+        country = driver.find_element_by_xpath('//a[@class="anchor__anchor--3DOSm wineLocationHeader__country--1RcW2"]').text
+    except Exception as e:
+        print(e)
+        
+    try:
+        wine_name = driver.find_element_by_xpath('//span[@class="vintage"]/a[@class="wine"]').text
+    except Exception as e:
+        print(e)
+        
+    try:
+        year_prep = driver.find_element_by_xpath('//span[@class="vintage"]').text
+        year = int(re.findall('\d+', year_prep)[0])
+    except Exception as e:
+        print(e)
+        
+    try:
+        overall_rating = float(driver.find_element_by_xpath('//div[@class="vivinoRating__averageValue--3Navj"]').text)
+    except Exception as e:
+        print(e)
+        
+    try:
+        overall_rating_count_prep = driver.find_element_by_xpath('//div[@class="vivinoRating__caption--3tZeS"]').text
+        overall_rating_count = int(re.findall('\d+', overall_rating_count_prep)[0])
+    except Exception as e:
+        print(e)
+        
+    try:
+        price_prep = driver.find_element_by_xpath('//span[@class="purchaseAvailability__currentPrice--3mO4u"]').text
+        price = float(re.findall('\d+', price_prep)[0])
+    except:
+        try:
+            price_prep = driver.find_element_by_xpath('//span[@class="purchaseAvailabilityPPC__amount--2_4GT"]').text
+            price = float(re.findall('\d+', price_prep)[0])
+        except Exception as e:
+            print(e)
+        
+    try:
+        driver.execute_script("window.scrollTo(0, 1080);")
+        driver.execute_script("window.scrollTo(0, 1080);")
+        driver.execute_script("window.scrollTo(0, 1080);")
+        driver.execute_script("window.scrollTo(0, 1080);")
+        driver.execute_script("window.scrollTo(0, 1080);")
+        time.sleep(2)
+        light_bold_prep = driver.find_element_by_xpath('//div[@class="indicatorBar__meter--2t_YL tasteStructure__progressBar--hjNb2"]/span[@class="indicatorBar__progress--3aXLX"]').get_attribute('style')
+        light_bold = float(re.findall('\d*\.?\d+',light_bold_prep)[1])
+    except Exception as e:
+        print(e)
+        
+    try:
+        smooth_tannic_prep = driver.find_element_by_xpath('//div[@class="indicatorBar__meter--2t_YL tasteStructure__progressBar--hjNb2"]/span[@class="indicatorBar__progress--3aXLX"]').get_attribute('style')
+        smooth_tannic = float(re.findall('\d*\.?\d+',smooth_tannic_prep)[1])
+    except Exception as e:
+        print(e)
+        
+    try:
+        dry_sweet_prep = driver.find_element_by_xpath('//div[@class="indicatorBar__meter--2t_YL tasteStructure__progressBar--hjNb2"]/span[@class="indicatorBar__progress--3aXLX"]').get_attribute('style')
+        dry_sweet = float(re.findall('\d*\.?\d+',dry_sweet_prep)[1])
+    except Exception as e:
+        print(e)
+        
+    try:
+        soft_acidic_prep = driver.find_element_by_xpath('//div[@class="indicatorBar__meter--2t_YL tasteStructure__progressBar--hjNb2"]/span[@class="indicatorBar__progress--3aXLX"]').get_attribute('style')
+        soft_acidic = float(re.findall('\d*\.?\d+',soft_acidic_prep)[1])
+    except Exception as e:
+        print(e)
 
+    # Define wine_dict and assign each element
+    wine_dict = {}
+    wine_dict['winery'] = winery
+    wine_dict['wine_type'] = wine_type
+    wine_dict['city'] = city
+    wine_dict['country'] = country
+    wine_dict['wine_name'] = wine_name
+    wine_dict['year'] = year
+    wine_dict['overall_rating'] = overall_rating
+    wine_dict['overall_rating_count'] = overall_rating_count
+    wine_dict['price'] = price
+    wine_dict['light_bold'] = light_bold
+    wine_dict['smooth_tannic'] = smooth_tannic
+    wine_dict['dry_sweet'] = dry_sweet
+    wine_dict['soft_acidic'] = soft_acidic
 
+    writer.writeheader()
+    writer.writerow(wine_dict)
 
-SCROLL_PAUSE_TIME = 1
-
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-	time.sleep(SCROLL_PAUSE_TIME)
-	new height = driver.execute_script("return document.body.scrollHeight")
-	if new_height == last_height:
-		break
-	last height = new_height
-
-
-all_urls
-
-for url in all_urls:
-
-
-
-
-
-While True:
-	
-	SCROLL_PAUSE_TIME = 1
-
-	last_height = driver.execute_script("return document.body.scrollHeight")
-
-	while True:
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		time.sleep(SCROLL_PAUSE_TIME)
-		new height = driver.execute_script("return document.body.scrollHeight")
-		if new_height == last_height:
-			break
-		last height = new_height
-
-	try:
-
-		# Elements that I want to scrape
-		winery = driver.find_element_by_xpath('//span[@class="headline"]/a[@class="winery"]').text
-		wine_type = driver.find_element_by_xpath('//span[@class="wineLocationHeader__wineType--14nrC"]').text
-		city = driver.find_element_by_xpath('//a[@class="anchor__anchor--3DOSm wineLocationHeader__region--1cbip"]').text
-		country = driver.find_element_by_xpath('//a[@class="anchor__anchor--3DOSm wineLocationHeader__country--1RcW2"]').text
-		wine_name = driver.find_element_by_xpath('//span[@class="vintage"]/a[@class="wine"]').text
-		year = int(driver.find_element_by_xpath('//span[@class="vintage"]').text[0])
-		overall_rating = int(driver.find_element_by_xpath('//div[@class="vivinoRating__averageValue--3Navj"]').text)
-		# I just need the number from here
-		overall_rating_count = int(driver.find_element_by_xpath('//div[@class="vivinoRating__caption--3tZeS"]').text)
-		# Seems like every page is different
-		try:
-			price = blah
-		except:
-			try:
-				price = bleh
-			except Exception as e:
-				print(e)
-		# How do I get an element? I need the 'left' element
-		light_bold = int(driver.find_element_by_xpath('//div[@class="indicatorBar__meter--2t_YL tasteStructure__progressBar--hjNb2"]/span[@class="indicatorBar__progress--3aXLX"]/style'))
-		smooth_tannic = 
-		dry_sweet = 
-		soft_acidic = 
-
-
-		wine_dict = {}
-
-		wine_dict['winery'] = winery
-		wine_dict['wine_type'] = wine_type
-		wine_dict['city'] = city
-		wine_dict['country'] = country
-		wine_dict['wine_name'] = wine_name
-		wine_dict['year'] = year
-		wine_dict['overall_rating'] = overall_rating
-		wine_dict['overall_rating_count'] = overall_rating_count
-		wine_dict['price'] = price
-		wine_dict['light_bold'] = light_bold
-		wine_dict['smooth_tannic'] = smooth_tannic
-		wine_dict['dry_sweet'] = dry_sweet
-		wine_dict['soft_acidic'] = soft_acidic
-
-		writer.writerow(wine_dict.values())
-
-
-
-
-
-except Exception as e:
-		print(e)
-		csv_file.close()
-		driver.close()
-		break
+csv_file.close()
